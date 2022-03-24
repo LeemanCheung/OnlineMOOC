e01200252c4dfd1aca16a23e8eb5d311dca79427
LeemanCheung <932502705@qq.com>
2022-03-24 19:23:36 +0800

    add login filter

---
+++ src/main/java/com/example/demoproject/handler/CustomExtHandler.java
@@ -0,0 +1,14 @@
+package com.example.demoproject.handler;
+
+import com.example.demoproject.utils.JsonData;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+@RestControllerAdvice
+public class CustomExtHandler {
+    @ExceptionHandler(value = Exception.class)
+    JsonData handlerException(Exception e, HttpServletRequest request){
+        return JsonData.buildError("server error",-2);
+    }
+}
